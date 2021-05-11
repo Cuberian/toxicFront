2d180d5a1c51146b11ff36153908ef7777a41156
Anastasia Lesiv <an_lesiv@mail.ru>
2021-05-11 17:55:03 +0500

    Set analysis page

--- src/components/AnalysisRequestForm/AnalysisRequestForm.js
+++ src/components/AnalysisRequestForm/AnalysisRequestForm.js
@@ -1,97 +1,118 @@
 import React, {useRef, useState} from 'react';
 import { useForm, useFieldArray } from "react-hook-form";
+import {$authMainHost} from "../../http";
 
 
-function AnalysisRequestForm(props) {
+function AnalysisRequestForm() {
+
+    const [sendResultNumber, setSendResultNumber] = useState()
 
     const { register, control, handleSubmit} = useForm();
-    const { fields, append, prepend, remove, swap, move, insert } = useFieldArray({
+    const { fields, append, remove } = useFieldArray({
         control,
         name: "objectInputs",
     });
 
     async function submitHandler(data) {
         console.log(data)
+        const { data: resData } = await $authMainHost.post('api/toxicity/analysis/request', data)
+        if(resData.result === 'success')
+        {
+            setSendResultNumber(resData.requestNumber)
+        }
     }
 
     return (
         <div className="flex w-full h-full mb-20">
-            <div className="flex flex-col container mx-auto px-5 py-4 space-y-4">
-                <p className="mx-auto text-2xl font-bold">Запрос на анализ</p>
-                <form onSubmit={handleSubmit(submitHandler)}>
-                    <div className="flex flex-col space-y-2">
-                        <label>Название запроса</label>
-                        <input className="border rounded-md px-3 py-2" type="text" {...register('reqName')}/>
-                    </div>
-                    <div className="flex flex-col space-y-2">
-                        <label>Обоснование запроса</label>
-                        <textarea className="border rounded-md px-3 py-2" {...register('reqReason')}/>
-                    </div>
-                    <div className="flex flex-col mb-3">
-                        <p className="font-semibold text-lg">Запрашиваемые объекты</p>
-                        <div className="flex flex-col space-y-3 py-3 w-full">
-                            {
-                                fields.map((field, index) => (
-                                    <div
-                                        key={'container_' + field.id}
-                                        className="flex md:flex-row flex-col md:space-x-3 md:space-y-0
-                                        md:px-5 py-5 md:border md:rounded-md border-b space-y-3 w-full">
-                                        <div className="flex flex-col space-y-2 md:w-2/6 w-full">
-                                            <label>Тип объекта</label>
-                                            <select
-                                                key={'type_' + field.id} // important to include key with field's id
-                                                className="border rounded-md px-3 py-2"
-                                                {...register(`objectInputs.${index}.objType`)}
-                                                defaultValue={''} // make sure to include defaultValue
-                                            >
-                                                <option value="post">Пост</option>
-                                                <option value="user">Пользователь</option>
-                                                <option value="group">Группа</option>
-                                            </select>
-                                        </div>
-                                        <div className="flex flex-col space-y-2 md:flex-grow w-full">
-                                            <label>ID объекта</label>
-                                            <input
-                                                key={'id_' + field.id} // important to include key with field's id
-                                                className="border rounded-md px-3 py-2"
-                                                {...register(`objectInputs.${index}.objId`)}
-                                                defaultValue={''} // make sure to include defaultValue
-                                            />
-                                        </div>
-                                        <div>
-                                            <svg
-                                                className="w-8 h-8 md:block hidden hover:bg-lime-400 rounded-full p-2"
-                                                onClick={() => remove(index)}
-                                                fill="currentColor"
-                                                viewBox="0 0 20 20"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                                            </svg>
-                                            <button className="bg-red-400 px-3 py-2 rounded-md w-1/4 text-white md:hidden"
-                                                    onClick={() => remove(index)}>
-                                                Убрать
-                                            </button>
-                                        </div>
-                                    </div>
-                                ))
-                            }
+            {
+               !sendResultNumber && <div className="flex flex-col container mx-auto px-5 py-4 space-y-4">
+                    <p className="mx-auto text-2xl font-bold">Запрос на анализ</p>
+                    <form onSubmit={handleSubmit(submitHandler)}>
+                        <div className="flex flex-col space-y-2">
+                            <label>Название запроса</label>
+                            <input className="border rounded-md px-3 py-2" type="text" {...register('reqName')}/>
                         </div>
-                        <button
-                            type="button"
-                            className="px-3 py-2 bg-blue-400 text-white rounded-md"
-                            onClick={() => append({ objType: "", objId: "" })}>Добавить объект</button>
-                    </div>
-                    <div className="flex md:flex-row flex-col md:space-x-3 md:space-y-0 space-y-3">
-                        <button
-                            className="disabled:opacity-50 px-3 py-2 bg-lime-400 text-white hover:bg-lime-600 rounded-md"
-                            disabled={fields.length === 0}>
-                            Отправить заявку
-                        </button>
-                        <button className="px-3 py-2 bg-red-400 text-white hover:bg-red-600 rounded-md">Отменить</button>
-                    </div>
+                        <div className="flex flex-col space-y-2">
+                            <label>Обоснование запроса</label>
+                            <textarea className="border rounded-md px-3 py-2" {...register('reqReason')}/>
+                        </div>
+                        <div className="flex flex-col mb-3">
+                            <p className="font-semibold text-lg">Запрашиваемые объекты</p>
+                            <div className="flex flex-col space-y-3 py-3 w-full">
+                                {
+                                    fields.map((field, index) => (
+                                        <div
+                                            key={'container_' + field.id}
+                                            className="flex md:flex-row flex-col md:space-x-3 md:space-y-0
+                                        md:px-5 py-5 md:border md:rounded-md border-b space-y-3 w-full">
+                                            <div className="flex flex-col space-y-2 md:w-2/6 w-full">
+                                                <label>Тип объекта</label>
+                                                <select
+                                                    key={'type_' + field.id} // important to include key with field's id
+                                                    className="border rounded-md px-3 py-2"
+                                                    {...register(`objectInputs.${index}.objType`)}
+                                                    defaultValue={''} // make sure to include defaultValue
+                                                >
+                                                    <option value="post">Пост</option>
+                                                    <option value="user">Пользователь</option>
+                                                    <option value="group">Группа</option>
+                                                </select>
+                                            </div>
+                                            <div className="flex flex-col space-y-2 md:flex-grow w-full">
+                                                <label>ID объекта</label>
+                                                <input
+                                                    key={'id_' + field.id} // important to include key with field's id
+                                                    className="border rounded-md px-3 py-2"
+                                                    {...register(`objectInputs.${index}.objId`)}
+                                                    defaultValue={''} // make sure to include defaultValue
+                                                />
+                                            </div>
+                                            <div>
+                                                <svg
+                                                    className="w-8 h-8 md:block hidden hover:bg-lime-400 rounded-full p-2"
+                                                    onClick={() => remove(index)}
+                                                    fill="currentColor"
+                                                    viewBox="0 0 20 20"
+                                                    xmlns="http://www.w3.org/2000/svg">
+                                                    <path fillRule="evenodd"
+                                                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                                          clipRule="evenodd"/>
+                                                </svg>
+                                                <button
+                                                    className="bg-red-400 px-3 py-2 rounded-md w-1/4 text-white md:hidden"
+                                                    onClick={() => remove(index)}>
+                                                    Убрать
+                                                </button>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                            <button
+                                type="button"
+                                className="px-3 py-2 bg-blue-400 text-white rounded-md"
+                                onClick={() => append({objType: "", objId: ""})}>Добавить объект
+                            </button>
+                        </div>
+                        <div className="flex md:flex-row flex-col md:space-x-3 md:space-y-0 space-y-3">
+                            <button
+                                className="disabled:opacity-50 px-3 py-2 bg-lime-400 text-white hover:bg-lime-600 rounded-md"
+                                disabled={fields.length === 0}>
+                                Отправить заявку
+                            </button>
+                            <button className="px-3 py-2 bg-red-400 text-white hover:bg-red-600 rounded-md">Отменить
+                            </button>
+                        </div>
 
-                </form>
-            </div>
+                    </form>
+                </div>
+            }
+            {
+                sendResultNumber && <div className="container pt-20 flex flex-col space-y-4 mx-auto">
+                    <p className="text-2xl font-semibold text-center">Запрос {sendResultNumber} успешно сформирован!</p>
+                    <p className="text-center">Вы можете сформировать новый запрос или перейти в личный кабинет, где будет отображаться текущий статус запроса.</p>
+                </div>
+            }
         </div>
     );
 }
--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -23,29 +23,26 @@ const Login = observer((props) => {
     const emailRef = useRef();
     const passwordRef = useRef();
 
-    // async function handleSubmit(e) {
-    //     e.preventDefault();
-    //     try {
-    //         setError('');
-    //         const response = await login(emailRef.current.value, passwordRef.current.value);
-    //         console.log(response);
-    //
-    //         user.setUser(response.user);
-    //         user.setIsAuth(true);
-    //         history.push('/main')
-    //     } catch (e) {
-    //         console.log(e);
-    //         setError('Ошибка авторизации!')
-    //     }
-    // }
+    async function loginHandler(data) {
+        try {
+            setError('');
+            const response = await login(data.email, data.password);
+            console.log(response);
 
-    const onSubmit = (data) => console.log(data);
+            user.setUser(response.user);
+            user.setIsAuth(true);
+            history.push('/main')
+        } catch (e) {
+            console.log(e);
+            setError('Ошибка авторизации!')
+        }
+    }
 
     return (
         <div className="flex justify-center items-center w-full">
             <div className="rounded-md p-5 lg:border flex flex-col justify-center 2xl:w-1/4 lg:w-1/3 w-full">
                 <h3 className="text-center mt-2 text-3xl font-semibold">Вход</h3>
-                <form className="mt-6" onSubmit={handleSubmit(onSubmit)}>
+                <form className="mt-6" onSubmit={handleSubmit(loginHandler)}>
                     <div className="flex flex-col content-center">
                         <label className="font-semibold mb-2 text-lg">Почта</label>
                         <input className="input-text" type="text"
